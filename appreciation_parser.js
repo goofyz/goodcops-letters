6567d6eb50ca741681e7e6f2a675ce82c3987274
Francis Chong <francis@ignition.hk>
2016-03-05 12:32:39 +0800

    complete parser

--- appreciation_parser.js
+++ appreciation_parser.js
@@ -2,31 +2,7 @@ var FeedParser = require('feedparser')
 var fs = require('fs')
 var _ = require('lodash')
 
-var file = "./data/appreciation_en.xml"
-var parser = new FeedParser([])
-var readStream = fs.createReadStream(file)
-
-readStream.on('open', () => {
-  readStream.pipe(parser)
-})
-
-parser.on('error', (error) => {
-  console.log(`${error}`)
-})
-
-parser.on('readable', function(error) {
-  var stream = this
-  var item
-  while(item = stream.read()) {
-    var output = parseItem(item)
-    if (output.officers) {
-      console.log(JSON.stringify(output, null, 2))
-    }
-  }
-})
-
-var policeRegexp = /praised in this letter (is|are)(.+)of(.+)/
-
+var policeRegexp = /praised in this letter (is|are)(.+?)of(.+)/
 function parseItem(item) {
   var title = item.title
   var text = item.description
@@ -37,12 +13,22 @@ function parseItem(item) {
   var matches = text.match(policeRegexp)
   if (matches) {
     officers = _.trim(matches[2])
+    officers = officers.split(/(,|and)/)
+      .filter((o) => o != "," && o != "and")
+      .map((o) => _.trim(o))
+
     var unitAndDivision = matches[3].split(",")
-    if (unitAndDivision.length == 2) {
-      unit = _.trim(unitAndDivision[0])
-      division = _.trim(unitAndDivision[1])
+    if (unitAndDivision.length >= 2) {
+      division = unitAndDivision.pop()
+      unit = unitAndDivision.join(",")
+      unit = _.trim(unit)
+      unit = _.replace(unit, '.', '')
+
+      division = _.trim(division)
+      division = _.replace(division, '.', '')
     } else {
-      unit = _.trim(unitAndDivision[0])
+      division = _.trim(unitAndDivision)
+      division = _.replace(division, '.', '')
     }
   }
 
@@ -54,3 +40,36 @@ function parseItem(item) {
     division: division,
   }
 }
+
+function parseAndExtractAppreciationLetter(file, callback) {
+  var parser = new FeedParser([])
+  var readStream = fs.createReadStream(file)
+
+  readStream.on('open', () => {
+    readStream.pipe(parser)
+  })
+
+  parser.on('error', (error) => {
+    callback(error);
+  })
+
+  parser.on('readable', function(error) {
+    var stream = this
+    var items = []
+    var item
+    while(item = stream.read()) {
+      var output = parseItem(item)
+      if (output.officers) {
+        items.push(output)
+        //JSON.stringify(output, null, 2)
+      }
+    }
+    callback(null, items)
+  })
+}
+
+parseAndExtractAppreciationLetter("./data/appreciation_en.xml", (e, letters) => {
+  letters.forEach((l) => {
+    console.log(JSON.stringify(l, null, 2))
+  })
+})
